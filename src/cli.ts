2d5804d185bab2cdd2d30cc01f8092f4a308a6a6
Franklin Shera <fshera96@gmail.com>
2022-11-23 07:57:32 +0300

    welcome conditions

--- src/cli.ts
+++ src/cli.ts
@@ -8,12 +8,16 @@ const sleep = async (ms: number = 500) =>
   new Promise((resolve) => setTimeout(resolve, ms));
 
 export async function cli(args: Args) {
-  if (args.includes("-h") || args.includes("--help")) {
+  if (
+    args.includes("-h") ||
+    args.includes("--help") ||
+    args.slice(2).length === 0
+  ) {
     welcome(true);
+  } else {
+    welcome();
+    await sleep();
   }
-
-  welcome();
-  await sleep();
 
   const options = parseArgs(args);
 
