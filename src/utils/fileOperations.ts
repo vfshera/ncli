eb754c8689e5c0fde600a0558bbd96257e284c25
Franklin Shera <fshera96@gmail.com>
2022-11-23 09:15:48 +0300

    creating new projects

--- src/utils/fileOperations.ts
+++ src/utils/fileOperations.ts
@@ -1,6 +1,52 @@
 import chalkAnimation from "chalk-animation";
+import { fileURLToPath } from "node:url";
+import path from "path";
 
-export function createProject(name: string) {
-  const anim = chalkAnimation.karaoke(`creating ${name}`);
-  setTimeout(() => anim.stop(), 4500);
+import ncp from "ncp";
+import { promisify } from "util";
+import { projectInstall } from "pkg-install";
+import Listr from "listr";
+import chalk from "chalk";
+
+const copy = promisify(ncp);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const templateDir = path.resolve(__dirname, "../template");
+
+// copy template files
+async function copyTemplate(targetDir: string) {
+  return copy(templateDir, targetDir, { clobber: false });
+}
+
+// install dependancies
+async function installDeps(targetDir: string) {
+  return projectInstall({
+    cwd: targetDir,
+  });
+}
+
+export async function createProject(name: string, install: boolean) {
+  const destination = path.join(process.cwd(), name);
+
+  const tasks = new Listr([
+    {
+      title: "Creating project...",
+      task: () => copyTemplate(destination),
+    },
+    {
+      title: "Installing dependancies...",
+      skip: () => !install,
+      task: () => installDeps(destination),
+    },
+  ]);
+
+  try {
+    await tasks.run();
+
+    console.log("%s Project ready", chalk.green.bold("DONE"));
+  } catch (error) {
+    console.log("%s Error occurred", chalk.red.bold("ERROR"));
+  }
 }
